b36adf2a619941e87612fe3ffc4f54d760133988
TrucLy-cloud <dly89680@gmail.com>
2025-10-14 00:17:19 +0700

    xxx

---
+++ Testing/Testing/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testing.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult MasterPage()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HistoryOrder()
+        {
+            return View();
+        }
+    }
+}
