c4d69d1cad0877b8b9fa01c6a2fbcf970be6da82
TrucLy-cloud <dly89680@gmail.com>
2025-10-21 21:58:50 +0700

    thêm testwith

---
+++ TestWith/TestWith/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWith.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult MasterPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestWith/TestWith/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestWith.Global" Language="C#" %>
